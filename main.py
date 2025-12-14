67cb9efb7d2aadb3bca1be9b56c358528f720ddd
saaleh2005 <salehbakhshi614@gmail.com>
2025-12-15 03:29:01 +0330

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from handlers.start import start_router
+
+async def main():
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(start_router)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
