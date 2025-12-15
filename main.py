7991396ccad56b45adb7bda658650784ab94a6d8
saaleh2005 <salehbakhshi614@gmail.com>
2025-12-15 10:18:25 +0330

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
+
 from handlers.start import start_router
 from handlers.articles import articles_router
-from handlers.search import search_router
 from handlers.quiz import quiz_router
+from handlers.search import search_router
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -12,8 +13,8 @@ async def main():
 
     dp.include_router(start_router)
     dp.include_router(articles_router)
-    dp.include_router(search_router)
     dp.include_router(quiz_router)
+    dp.include_router(search_router)
 
     await dp.start_polling(bot)
 
